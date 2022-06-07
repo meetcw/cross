b9e134dd209454890e33b51f46eaec9994428d6b
meetcw <meetcw@outlook.com>
2022-06-08 04:45:56 +0800

    .

--- trojan-go/install.sh
+++ trojan-go/install.sh
@@ -2,9 +2,9 @@
 export $(grep -v '^#' .env | xargs)
 
 mkdir -p /etc/nginx
-envsubst < ./nginx/nginx.conf > /etc/nginx/nginx.conf
+envsubst < ./nginx/nginx.conf | sed -e 's/§/$/g' > /etc/nginx/nginx.conf
 mkdir -p /etc/nginx/sites-available
-envsubst < ./nginx/sites-available/default > /etc/nginx/sites-available/default
+envsubst < ./nginx/sites-available/default | sed -e 's/§/$/g' > /etc/nginx/sites-available/default
 
 cp -r ./virtual-site /var/www/virtual-site
 envsubst < ./virtual-site/index.html > /var/www/virtual-site/index.html
